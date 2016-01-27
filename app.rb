74b90970b0b98ee16c976580a177e161072a0fd1
David P <david@davidpena.es>
2016-01-27 11:06:17 +0100

    add buy form

--- app.rb
+++ app.rb
@@ -4,7 +4,30 @@ require 'sinatra'
 SITE_TITLE = "CineTicket"
 SITE_DESCRIPTION = "Tu entrada de cine online"
 
+FILMS = [
+  [1, 'Peli1', 2],
+  [2, 'Peli2', 3.5]
+]
+
+tickets = []
+
 get '/' do
   @title = 'Inicio'
+  @tickets = tickets
   erb :home
+end
+
+get '/buy/?' do
+  @films = FILMS
+  @title = 'Compra'
+  erb :buy
+end
+
+post '/buy/?' do
+  tickets << [params[:name], params[:phone], params[:email], params[:film] ]
+  redirect '/'
+end
+
+after do
+  tickets.each{|ticket| p ticket.join(' | ')}
 end
